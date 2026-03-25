24420eb0c3386bf7c7de9f90cb9aa2c6b8b443f5
floresgisselle <floresgisselle.2008@gmail.com>
2026-03-24 23:36:51 -0600

    Add files via upload

---
+++ Notafinal/Notafinal/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Notafinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtExamen = new System.Windows.Forms.TextBox();
+            this.txtAcumulativo = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rbPresencial = new System.Windows.Forms.RadioButton();
+            this.rbDistancia = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre de estudiante";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nota acumulativa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Examen";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(282, 71);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(429, 26);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtExamen
+            // 
+            this.txtExamen.Location = new System.Drawing.Point(282, 196);
+            this.txtExamen.Name = "txtExamen";
+            this.txtExamen.Size = new System.Drawing.Size(429, 26);
+            this.txtExamen.TabIndex = 4;
+            // 
+            // txtAcumulativo
+            // 
+            this.txtAcumulativo.Location = new System.Drawing.Point(282, 137);
+            this.txtAcumulativo.Name = "txtAcumulativo";
+            this.txtAcumulativo.Size = new System.Drawing.Size(429, 26);
+            this.txtAcumulativo.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(267, 281);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(68, 42);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Limpiar";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(572, 281);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(68, 42);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Calcular Resultado";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // rbPresencial
+            // 
+            this.rbPresencial.AutoSize = true;
+            this.rbPresencial.Location = new System.Drawing.Point(19, 20);
+            this.rbPresencial.Name = "rbPresencial";
+            this.rbPresencial.Size = new System.Drawing.Size(107, 24);
+            this.rbPresencial.TabIndex = 10;
+            this.rbPresencial.TabStop = true;
+            this.rbPresencial.Text = "Presencial";
+            this.rbPresencial.UseVisualStyleBackColor = true;
+            // 
+            // rbDistancia
+            // 
+            this.rbDistancia.AutoSize = true;
+            this.rbDistancia.Location = new System.Drawing.Point(155, 20);
+            this.rbDistancia.Name = "rbDistancia";
+            this.rbDistancia.Size = new System.Drawing.Size(100, 24);
+            this.rbDistancia.TabIndex = 11;
+            this.rbDistancia.TabStop = true;
+            this.rbDistancia.Text = "Distancia";
+            this.rbDistancia.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbDistancia);
+            this.groupBox1.Controls.Add(this.rbPresencial);
+            this.groupBox1.Location = new System.Drawing.Point(310, 355);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(277, 69);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Jornada";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtAcumulativo);
+            this.Controls.Add(this.txtExamen);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtExamen;
+        private System.Windows.Forms.TextBox txtAcumulativo;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rbPresencial;
+        private System.Windows.Forms.RadioButton rbDistancia;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Notafinal/Notafinal/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notafinal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            double acumulativo, examen, notaFinal;
+            string jornada = "";
+            string resultado = "";
+
+
+            if (!double.TryParse(txtAcumulativo.Text, out acumulativo) ||
+                !double.TryParse(txtExamen.Text, out examen))
+            {
+                MessageBox.Show("Ingrese valores numéricos válidos");
+                return;
+            }
+
+            if (rbPresencial.Checked)
+                jornada = "Presencial";
+            else if (rbDistancia.Checked)
+                jornada = "Distancia";
+            else
+            {
+                MessageBox.Show("Seleccione una jornada");
+                return;
+            }
+
+            notaFinal = acumulativo + examen;
+
+
+            if (notaFinal >= 60)
+                resultado = "Aprobado";
+            else
+                resultado = "Reprobado";
+
+
+            MessageBox.Show(
+                "Nombre: " + nombre +
+                "\nJornada: " + jornada +
+                "\nNota final: " + notaFinal +
+                "\nResultado: " + resultado
+            );
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            txtNombre.Clear();
+            txtAcumulativo.Clear();
+            txtExamen.Clear();
+            rbPresencial.Checked = false;
+            rbDistancia.Checked = false;
+            txtNombre.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();  
+        }
+    }
+}
